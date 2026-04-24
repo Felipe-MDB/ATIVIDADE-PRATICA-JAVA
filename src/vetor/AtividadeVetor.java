b81d10655f027f87a6183edffec7bd497a2f1b36
Felipe-MDB <femarques.brito@outlook.com>
2026-04-24 18:06:39 -0300

    Atividade de vetor

---
+++ src/vetor/AtividadeVetor.java
@@ -0,0 +1,40 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class AtividadeVetor {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+				// Variaveis
+				int vetor[] = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6,};
+				int numeroProcurado, posicao = -1;
+				
+					System.out.print("Digite o número que você deseja encontrar: ");
+				numeroProcurado = leia.nextInt();
+			
+				// Laço de repetição
+				for(int i = 0; i < 10; i++) {
+					
+					// Laço condicional para verificar se o número atual é o que buscamos
+		            if (vetor[i] == numeroProcurado) {		            	
+		                posicao = i; // Guarda o índice (posição)	
+					
+				}
+					
+			}
+		
+		            // Saída de dados 
+		            if (posicao != -1) {
+		                System.out.println("O número " + numeroProcurado + " está localizado na posição: " + posicao);
+		            } else {
+		                System.out.println("O número " + numeroProcurado + " não foi encontrado!");
+		            }	
+		        
+		        leia.close();    
+		            
+	}
+
+}
